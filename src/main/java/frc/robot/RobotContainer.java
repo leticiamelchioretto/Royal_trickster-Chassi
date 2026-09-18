573361ccdc028e0dd67fbe135bd3d9752225b39a
Leticia Melchioretto <leticia_melchiore12@estudante.sesisenai.org.br>
2026-09-18 16:54:19 -0300

    Mudanças nos eixos do Joystic

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -28,8 +28,8 @@ public class RobotContainer {
     private void configurarComandosPadrao() {
         drive.setDefaultCommand(new RunCommand(
             () -> drive.arcadeDrive(
-                -controle.getLeftY(),
-                 controle.getRightX()
+                -controle.getLeftX(),
+                controle.getRightY()
             ),
             drive
         ));
